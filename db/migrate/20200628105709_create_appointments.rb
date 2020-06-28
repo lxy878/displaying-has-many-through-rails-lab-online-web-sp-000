2d6101a54e61647573c2156716cdc8386f248fe0
Xiaoyi Liu <lxy878@gmail.com>
2020-06-28 11:08:43 +0000

    Automatically backed up by Learn

--- db/migrate/20200628105709_create_appointments.rb
+++ db/migrate/20200628105709_create_appointments.rb
@@ -1,7 +1,7 @@
 class CreateAppointments < ActiveRecord::Migration[5.0]
   def change
     create_table :appointments do |t|
-      t.date :appointment_datetime
+      t.datetime :appointment_datetime
       t.integer :patient
       t.integer :doctor
       t.timestamps
