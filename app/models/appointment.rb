f72af69eb85bbd5986aae80455c0eb529e0f5694
Xiaoyi Liu <lxy878@gmail.com>
2020-06-28 12:47:11 +0000

    Automatically backed up by Learn

--- app/models/appointment.rb
+++ app/models/appointment.rb
@@ -3,6 +3,6 @@ class Appointment < ApplicationRecord
   belongs_to :patient
 
   def datetime_form
-    self.appointment_datetime.strftime('%B %d, %Y at%l:%M')
+    self.appointment_datetime.strftime('%B %d, %Y at%k:%M')
   end
 end
